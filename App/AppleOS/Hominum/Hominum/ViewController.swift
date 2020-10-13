58d79f4760a4b20847e102548aa81733852ee6b9
MarioAndF <marioandf@gmail.com>
2020-10-12 23:31:56 -0600

    Updated JSON keys

---
+++ App/AppleOS/Hominum/Hominum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hominum
+//
+//  Created by Mario Flores on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
